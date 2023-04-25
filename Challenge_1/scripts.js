09c51e1f43b99fe0cbf023e4dd9b6e3b54219df1
sanda <sandasomlenze1997@gmail.com>
2023-04-25 10:29:33 +0200

    First Commit

---
+++ Challenge_1/scripts.js
@@ -0,0 +1,28 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+// const logTwice = () => {
+//   console(parameter)
+//   console(parameter)
+// }
+
+const logTwice = () => {
+  console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+logTwice()
+logTwice()
+
+// const firstName = () => 'John' ;
+// const age = () => '35';
+// const hobby = () => 'Coding';
+
+// const logTwice =   
+//   console.log(`Hello, ${firstName()} (${age()}). I love ${hobby()}!`)
+//   console.log(`Hello, ${firstName()} (${age()}). I love ${hobby()}!`)
+
+
+
+
+//   console.log(`Hello, ${firstName()} (${age()}). I love ${hobby()}!`)
